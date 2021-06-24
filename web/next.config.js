413534e8415d663266ab1f852cab14d3856e55ca
Jack <jackbaude@gmail.com>
2021-06-24 14:18:21 -0500

    Final release

--- web/next.config.js
+++ web/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:8000/:path*' // Proxy to Backend
+                destination: 'http://127.0.0.1:8000/:path*' // Proxy to Backend
             }
         ]
     }

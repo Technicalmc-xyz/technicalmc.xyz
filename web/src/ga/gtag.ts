dbdf7a75a7f427d957ba0241fd8af85d5c013724
Jack <jackbaude@gmail.com>
2021-07-05 20:43:32 -0500

    Fixed type validity

--- web/src/ga/gtag.ts
+++ web/src/ga/gtag.ts
@@ -1,9 +1,10 @@
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS as string, {
     page_path: url,
   });
-};
+}
+
 
 type GTagEvent = {
   action: string;

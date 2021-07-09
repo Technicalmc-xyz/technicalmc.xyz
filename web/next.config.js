acc7811dd9822903072aa94af0f1e682fddac3f0
Jack <jackbaude@gmail.com>
2021-07-09 14:55:15 -0500

    Add linting and prettier
    Added eslint and prettier to the project. However, for now i am ignoring it on the build because there are so many issues.

--- web/next.config.js
+++ web/next.config.js
@@ -2,9 +2,17 @@ module.exports = {
     async rewrites() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'http://127.0.0.1:8000/:path*' // Proxy to Backend
-            }
-        ]
-    }
-}
+                source: "/api/:path*",
+                destination: "http://127.0.0.1:8000/:path*", // Proxy to Backend
+            },
+        ];
+    },
+    eslint: {
+        // Warning: Dangerously allow production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        domains: ["cdn.discordapp.com"],
+    },
+};
